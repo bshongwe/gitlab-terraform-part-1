5fdb86b8800f17f2ca112596b8b48ca325c15c3c
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-23 09:16:36 +0200

    Update: Jenkins CI-CD automation that runs tests, builds Docker images and deploys to multi-ENVs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
 /** JEST: if needed, take precaution */
 module.exports = {
+	testMatch: [
+		"**/ __tests__/**/*.js?(x)",
+		"**/?(*.)+(spec|test).js?(x)"
+	],
+	verbose: true,
+	clearMocks: true,
+	setupFiles: [
+		"<rootDir>/setupTests.js"
+	],
+
 	testEnvironment: 'node',
-	verbose: true
+	collectCoverage: true,
+	coverageDirectory: "coverage",
+	coverageReporters: ["text", "html"],
+
+	testPathIgnorePatterns: [
+		"node_modules",
+		"/dist/"
+	],
+
+	moduleNameMapper: {
+		"\\.(css|less)$": "identity-odj-proxy",
+		"\\.(gif|ttf|eot|svg|png)$": "<rootDir>/ __mocks__fileMock.js"
+	}
 };
